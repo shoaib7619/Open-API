c4aac130a2e0fc17a8e019be9e0de0be2a1fb326
Muhammad Shoaib <94863313+shoaib7619@users.noreply.github.com>
2024-04-11 16:37:11 +0500

    init

--- src/pages/header.jsx
+++ src/pages/header.jsx
@@ -10,12 +10,12 @@ function Header() {
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="me-auto">
-              <NavLink to="/posts" className="nav-link" activeClassName="active">Posts</NavLink>
-              <NavLink to="/comments" className="nav-link" activeClassName="active">Comments</NavLink>
-              <NavLink to="/albums" className="nav-link" activeClassName="active">Albums</NavLink>
-              <NavLink to="/photos" className="nav-link" activeClassName="active">Photos</NavLink>
-              <NavLink to="/todos" className="nav-link" activeClassName="active">Todos</NavLink>
-              <NavLink to="/users" className="nav-link" activeClassName="active">Users</NavLink>
+              <NavLink to="/posts" className="nav-link" activeclassname="active">Posts</NavLink>
+              <NavLink to="/comments" className="nav-link" activeclassname="active">Comments</NavLink>
+              <NavLink to="/albums" className="nav-link" activeclassname="active">Albums</NavLink>
+              <NavLink to="/photos" className="nav-link" activeclassname="active">Photos</NavLink>
+              <NavLink to="/todos" className="nav-link" activeclassname="active">Todos</NavLink>
+              <NavLink to="/users" className="nav-link" activeclassname="active">Users</NavLink>
             </Nav>
           </Navbar.Collapse>
         </Container>
